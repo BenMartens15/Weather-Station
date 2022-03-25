a678dc0eada4a09ace2d54770e88794a63572d6a
Ben Martens <2benmartens15@gmail.com>
2022-03-25 09:06:25 -0400

    Functional weather station
     - Reads temperature and humidity from sensor and displays readings on the LCD
     - Keypad can be used to switch between displaying temperature, humidity, or both

--- lib/keypad/keypad.c
+++ lib/keypad/keypad.c
@@ -14,13 +14,17 @@ static uint8_t column_port;
 static uint8_t column_pins[3];
 static uint8_t row_port;
 static uint8_t row_pins[4];
-uint8_t keypad_key_pressed;
+static uint8_t keypad_key_pressed;
 
 static void debounce() { // delay for 10ms to debounce the keypad buttons
     int i, j;
-    for(i = 0 ; i < 10; i++)
+    for(i = 0 ; i < 50; i++)
         for(j = 0; j < 3180; j++)
             {}  /* execute NOP for 1ms */
+}
+
+uint8_t get_keypad_key_pressed() {
+    return keypad_key_pressed;
 }
 
 void keypad_init(uint8_t c_port, uint8_t r_port, uint8_t* c_pins, uint8_t* r_pins) {
@@ -96,6 +100,7 @@ uint8_t keypad_check_key() {
         GPIO_write_pin(&keypad_rows, GPIO_PIN_LOW);
     }
 
+    debounce(); // not really sure why this delay is needed, but this logic block always returns 1 if the delay isn't here
     if (row_pressed == 0) {
         if (column_pressed == 0) {
             return 1;
@@ -143,47 +148,81 @@ uint8_t keypad_check_key() {
     return 13;
 }
 
-/*
- * GPIO interrupt handlers
- */
 void GPIO_porta_handler() {
+    GPIO_PORTA_IM_R &= ~(1 << column_pins[0]); // mask the interrupts to prevent firing multiple times
+    GPIO_PORTA_IM_R &= ~(1 << column_pins[1]);
+    GPIO_PORTA_IM_R &= ~(1 << column_pins[2]);
     keypad_key_pressed = keypad_check_key();
     GPIO_PORTA_ICR_R |= (1 << column_pins[0]);
     GPIO_PORTA_ICR_R |= (1 << column_pins[1]);
     GPIO_PORTA_ICR_R |= (1 << column_pins[2]);
+    GPIO_PORTA_IM_R |= (1 << column_pins[0]); // unmask the interrupts
+    GPIO_PORTA_IM_R |= (1 << column_pins[1]);
+    GPIO_PORTA_IM_R |= (1 << column_pins[2]);
 }
 
 void GPIO_portb_handler() {
+    GPIO_PORTB_IM_R &= ~(1 << column_pins[0]); // mask the interrupts to prevent firing multiple times
+    GPIO_PORTB_IM_R &= ~(1 << column_pins[1]);
+    GPIO_PORTB_IM_R &= ~(1 << column_pins[2]);
     keypad_key_pressed = keypad_check_key();
     GPIO_PORTB_ICR_R |= (1 << column_pins[0]);
     GPIO_PORTB_ICR_R |= (1 << column_pins[1]);
     GPIO_PORTB_ICR_R |= (1 << column_pins[2]);
+    GPIO_PORTB_IM_R |= (1 << column_pins[0]); // unmask the interrupts
+    GPIO_PORTB_IM_R |= (1 << column_pins[1]);
+    GPIO_PORTB_IM_R |= (1 << column_pins[2]);
 }
 
 void GPIO_portc_handler() {
+    GPIO_PORTC_IM_R &= ~(1 << column_pins[0]); // mask the interrupts to prevent firing multiple times
+    GPIO_PORTC_IM_R &= ~(1 << column_pins[1]);
+    GPIO_PORTC_IM_R &= ~(1 << column_pins[2]);
     keypad_key_pressed = keypad_check_key();
     GPIO_PORTC_ICR_R |= (1 << column_pins[0]);
     GPIO_PORTC_ICR_R |= (1 << column_pins[1]);
     GPIO_PORTC_ICR_R |= (1 << column_pins[2]);
+    GPIO_PORTC_IM_R |= (1 << column_pins[0]); // unmask the interrupts
+    GPIO_PORTC_IM_R |= (1 << column_pins[1]);
+    GPIO_PORTC_IM_R |= (1 << column_pins[2]);
 }
 
 void GPIO_portd_handler() {
+    GPIO_PORTD_IM_R &= ~(1 << column_pins[0]); // mask the interrupts to prevent firing multiple times
+    GPIO_PORTD_IM_R &= ~(1 << column_pins[1]);
+    GPIO_PORTD_IM_R &= ~(1 << column_pins[2]);
     keypad_key_pressed = keypad_check_key();
     GPIO_PORTD_ICR_R |= (1 << column_pins[0]);
     GPIO_PORTD_ICR_R |= (1 << column_pins[1]);
     GPIO_PORTD_ICR_R |= (1 << column_pins[2]);
+    GPIO_PORTD_IM_R |= (1 << column_pins[0]); // unmask the interrupts
+    GPIO_PORTD_IM_R |= (1 << column_pins[1]);
+    GPIO_PORTD_IM_R |= (1 << column_pins[2]);
 }
 
 void GPIO_porte_handler() {
+    GPIO_PORTE_IM_R &= ~(1 << column_pins[0]); // mask the interrupts to prevent firing multiple times
+    GPIO_PORTE_IM_R &= ~(1 << column_pins[1]);
+    GPIO_PORTE_IM_R &= ~(1 << column_pins[2]);
     keypad_key_pressed = keypad_check_key();
     GPIO_PORTE_ICR_R |= (1 << column_pins[0]);
     GPIO_PORTE_ICR_R |= (1 << column_pins[1]);
     GPIO_PORTE_ICR_R |= (1 << column_pins[2]);
+    GPIO_PORTE_IM_R |= (1 << column_pins[0]); // unmask the interrupts
+    GPIO_PORTE_IM_R |= (1 << column_pins[1]);
+    GPIO_PORTE_IM_R |= (1 << column_pins[2]);
 }
 
 void GPIO_portf_handler() {
+    GPIO_PORTF_IM_R &= ~(1 << column_pins[0]); // mask the interrupts to prevent firing multiple times
+    GPIO_PORTF_IM_R &= ~(1 << column_pins[1]);
+    GPIO_PORTF_IM_R &= ~(1 << column_pins[2]);
     keypad_key_pressed = keypad_check_key();
     GPIO_PORTF_ICR_R |= (1 << column_pins[0]);
     GPIO_PORTF_ICR_R |= (1 << column_pins[1]);
     GPIO_PORTF_ICR_R |= (1 << column_pins[2]);
+    GPIO_PORTF_IM_R |= (1 << column_pins[0]); // unmask the interrupts
+    GPIO_PORTF_IM_R |= (1 << column_pins[1]);
+    GPIO_PORTF_IM_R |= (1 << column_pins[2]);
 }
+
