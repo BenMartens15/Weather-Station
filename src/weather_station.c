611cc0f520b77c6b083aa73e6c05f0904dd72307
Ben Martens <2benmartens15@gmail.com>
2022-04-08 09:52:53 -0400

    Add motor rotate keys to menu

--- src/weather_station.c
+++ src/weather_station.c
@@ -16,12 +16,8 @@
 
 #define STEPS_PER_REV   200
 
-void delay_ms(uint16_t time_ms) {
-    uint16_t i, j;
-    for(i = 0 ; i < time_ms; i++)
-        for(j = 0; j < 3180; j++)
-            {}  /* execute NOP for 1ms */
-}
+void display_menu();
+void delay_ms(uint16_t time_ms);
 
 int main() {
     uint8_t keypad_column_pins[] = {GPIO_PIN_NUM_3, GPIO_PIN_NUM_2, GPIO_PIN_NUM_4};
@@ -33,32 +29,12 @@ int main() {
     baro_sensor_init(SPI2, SPI_SS_PORT_E, SPI_SS_PIN_0);
     lux_sensor_init(I2C1);
     keypad_init(GPIOA, GPIOC, keypad_column_pins, keypad_row_pins);
+    stepper_init(200, GPIOE, GPIO_PIN_NUM_3, GPIOF, GPIO_PIN_NUM_1, GPIOF, GPIO_PIN_NUM_2, GPIOF, GPIO_PIN_NUM_3);
     delay_ms(500);
 
     // display the menu until a key is pressed
     while(get_first_key_pressed() == 0) {
-        LCD_clear();
-        LCD_display_string("1: Temperature");
-        LCD_move_cursor(1, 0);
-        LCD_display_string("2: Humidity");
-        delay_ms(750);
-        if (get_first_key_pressed()) {
-            break;
-        }
-        LCD_clear();
-        LCD_display_string("3: Baro Pressure");
-        LCD_move_cursor(1, 0);
-        LCD_display_string("4: Luminance");
-        delay_ms(750);
-        if (get_first_key_pressed()) {
-            break;
-        }
-        LCD_clear();
-        LCD_display_string("5: Back to Menu");
-        delay_ms(750);
-        if (get_first_key_pressed()) {
-            break;
-        }
+        display_menu();
     }
 
 
@@ -67,12 +43,12 @@ int main() {
         uint32_t ht_readings_integer[2]; // the integer portion of the humidity and temperature readings
         uint32_t ht_readings_decimal[2]; // the decimal portion of the humidity and temperature readings
 
-        char display_output[16];
+        char display_output[17];
         memset(display_output, 0, sizeof(display_output)); // clear display_output
         if (get_keypad_key_pressed() == 1) {
             temp_sensor_measure(humidity_temp);
             temp_sensor_to_decimal(humidity_temp, ht_readings_integer, ht_readings_decimal);
-            snprintf(display_output, 16, "Temp: %d.%d C", ht_readings_integer[1], ht_readings_decimal[1]);
+            snprintf(display_output, 17, "Temp: %d.%d C", ht_readings_integer[1], ht_readings_decimal[1]);
             LCD_move_cursor(0, 0);
             LCD_clear();
             LCD_display_string(display_output);
@@ -80,60 +56,90 @@ int main() {
         else if(get_keypad_key_pressed() == 2) {
             temp_sensor_measure(humidity_temp);
             temp_sensor_to_decimal(humidity_temp, ht_readings_integer, ht_readings_decimal);
-            snprintf(display_output, 16, "RH: %d.%d%%", ht_readings_integer[0], ht_readings_decimal[0]);
+            snprintf(display_output, 17, "RH: %d.%d%%", ht_readings_integer[0], ht_readings_decimal[0]);
             LCD_move_cursor(0, 0);
             LCD_clear();
             LCD_display_string(display_output);
         }
         else if(get_keypad_key_pressed() == 3) {
             float barometric_pressure = baro_measure_pressure();
-            snprintf(display_output, 16, "BP: %0.1fkPa", barometric_pressure);
+            snprintf(display_output, 17, "BP: %0.1fkPa", barometric_pressure);
             LCD_move_cursor(0, 0);
             LCD_clear();
             LCD_display_string(display_output);
         }
         else if(get_keypad_key_pressed() == 4) {
             float lux = lux_sensor_read_lux();
-            snprintf(display_output, 16, "LUM: %0.1f lux", lux);
+            snprintf(display_output, 17, "LUM: %0.1f lux", lux);
             LCD_move_cursor(0, 0);
             LCD_clear();
             LCD_display_string(display_output);
         }
         else if(get_keypad_key_pressed() == 5) {
             while(1) {
-                LCD_clear();
-                LCD_display_string("1: Temperature");
-                LCD_move_cursor(1, 0);
-                LCD_display_string("2: Humidity");
-                delay_ms(750);
-                if (get_first_key_pressed()) {
-                    break;
+                if(get_first_key_pressed() == 0) {
+                    display_menu();
                 }
-                LCD_clear();
-                LCD_display_string("3: Baro Pressure");
-                LCD_move_cursor(1, 0);
-                LCD_display_string("4: Luminance");
-                delay_ms(750);
-                if (get_first_key_pressed()) {
-                    break;
-                }
-                LCD_clear();
-                LCD_display_string("5: Back to Menu");
-                delay_ms(750);
-                if (get_first_key_pressed()) {
+                else {
                     break;
                 }
             }
         }
         else if(get_keypad_key_pressed() == 6) {
-            snprintf(display_output, 16, "Motor rotating");
-            LCD_move_cursor(0, 0);
             LCD_clear();
-            LCD_display_string(display_output);
-            stepper_rotate(DIRECTION_CLOCKWISE, 5, 600);
+            LCD_display_string("Motor rotating");
+            LCD_move_cursor(1, 0);
+            LCD_display_string("clockwise");
+            stepper_rotate(DIRECTION_CLOCKWISE, 5, 100);
+        }
+        else if(get_keypad_key_pressed() == 7) {
+            LCD_clear();
+            LCD_display_string("Motor rotating");
+            LCD_move_cursor(1, 0);
+            LCD_display_string("counterclockwise");
+            stepper_rotate(DIRECTION_COUNTER_CLOCKWISE, 5, 100);
         }
         delay_ms(250);
     }
 }
 
+void display_menu() {
+    LCD_clear();
+    LCD_display_string("1: Temperature");
+    LCD_move_cursor(1, 0);
+    LCD_display_string("2: Humidity");
+    delay_ms(750);
+    if (get_first_key_pressed()) {
+        return;
+    }
+    LCD_clear();
+    LCD_display_string("3: Baro Pressure");
+    LCD_move_cursor(1, 0);
+    LCD_display_string("4: Luminance");
+    delay_ms(750);
+    if (get_first_key_pressed()) {
+        return;
+    }
+    LCD_clear();
+    LCD_display_string("5: Back to Menu");
+    delay_ms(750);
+    if (get_first_key_pressed()) {
+        return;
+    }
+    LCD_clear();
+    LCD_display_string("6: Rotate CW");
+    LCD_move_cursor(1, 0);
+    LCD_display_string("7: Rotate CCW");
+    delay_ms(750);
+    if (get_first_key_pressed()) {
+        return;
+    }
+}
+
+void delay_ms(uint16_t time_ms) {
+    uint16_t i, j;
+    for(i = 0 ; i < time_ms; i++)
+        for(j = 0; j < 3180; j++)
+            {}  /* execute NOP for 1ms */
+}
 
